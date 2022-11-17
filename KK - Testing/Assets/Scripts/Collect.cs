736447f3a78e8b45dcdd06e1e7f82c8991362f21
Kelton <95721065+codeninjaskelton@users.noreply.github.com>
2022-11-17 17:58:40 -0500

    Block bug

--- KK - Testing/Assets/Scripts/Collect.cs
+++ KK - Testing/Assets/Scripts/Collect.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Collect : MonoBehaviour
 {
+    private GameObject portal;
+
+    private void Start()
+    {
+        portal = GameObject.FindGameObjectWithTag("Portal");
+        portal.SetActive(false);
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            portal.SetActive(true);
             gameObject.SetActive(false);
         }
     }
