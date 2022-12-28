e60f4af9e4c5169ef3721d41e0d59729bb920eea
Kelton <95721065+codeninjaskelton@users.noreply.github.com>
2022-12-28 15:00:19 -0500

    still need to fix the camera zoom

--- KK - Testing/Assets/Scripts/CinemachineZoom.cs
+++ KK - Testing/Assets/Scripts/CinemachineZoom.cs
@@ -6,11 +6,25 @@ using Cinemachine;
 public class CinemachineZoom : MonoBehaviour
 {
     private CinemachineVirtualCamera cm;
+    public float startZoom;
+    public float endZoom;
+    private float elasped = 0.0f;
+    public float duration;
 
     private void Start()
     {
         cm = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>();
+        cm.m_Lens.OrthographicSize = startZoom;
+        EndZoom();
     }
 
-
+    public IEnumerator EndZoom()
+    {
+        yield return new WaitForSeconds(3);
+        while (cm.m_Lens.OrthographicSize > endZoom)
+        {
+            elasped += Time.deltaTime / duration;
+            cm.m_Lens.OrthographicSize = Mathf.Lerp(startZoom, endZoom, elasped);
+        }
+    }
 }
