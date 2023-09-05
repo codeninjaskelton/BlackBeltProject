8ec47f18c10730f3dc3d6ce1f890b18c74584b40
Kelton <95721065+codeninjaskelton@users.noreply.github.com>
2023-09-05 18:00:31 -0400

    Bean Game

--- KK - Testing/Assets/Scripts/DeletePlayerPrefsScript.cs
+++ KK - Testing/Assets/Scripts/DeletePlayerPrefsScript.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEditor;
-
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
 public class DeletePlayerPrefsScript : EditorWindow
 {
+
     [MenuItem("Window/Delete PlayerPrefs (All)")]
     static void DeleteAllPlayerPrefs()
     {
         PlayerPrefs.DeleteAll();
     }
+
 }
+#endif
